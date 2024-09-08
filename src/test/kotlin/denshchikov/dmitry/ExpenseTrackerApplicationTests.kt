365937173288d55b97b4c1bbbb9957dcddfa30b6
Dmitry Denshchikov <mr.dd33@yandex.ru>
2024-09-08 16:24:56 +0200

    Initial commit

---
+++ src/test/kotlin/denshchikov/dmitry/ExpenseTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package denshchikov.dmitry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
